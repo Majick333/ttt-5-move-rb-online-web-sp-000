876ab2f005e40f012d7711f8e79be8014b84c3b5
Ryan M <rmccauleymusic@gmail.com>
2019-10-10 01:04:02 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,8 @@ def input_to_index (user_input)
   user_input.to_i - 1
 end
 
-char = "X" || "O"
 
-def move(board,index,char)
-  board[index] = char
+def move(board,index,value)
+  board[index] = value
 end
+
